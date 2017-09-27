4997182661fd1559dc3adbfcb6f1c7411c7df97e
Rustin Gibbs <rustingibbs@Rustins-iMac.local>
2017-09-27 09:08:56 -0600

    first files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/eko.html'));
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
